89208196e28628cd3a181855c91efc24c677d850
Bryan Yap <bryan.yap@alliants.com>
2016-09-27 15:43:58 +0100

    Refactor the Page router
    - Refactored the Page router into a separate class and instantiate it as
      a singleton

--- web/app/page.jsx
+++ web/app/page.jsx
@@ -1,5 +1,7 @@
 import preact from 'preact'
 
+let router = null // instantiated later as a singleton
+
 export default class Page extends preact.Component {
   render() {
     if (this.props.children.length !== 1) {
@@ -16,7 +18,7 @@ export default class Page extends preact.Component {
   componentDidMount() {
     this.updatePageMeta(this.props)
 
-    mountRouter(this)
+    router.mountPage(this)
   }
 
   updatePageMeta(props) {
@@ -24,66 +26,92 @@ export default class Page extends preact.Component {
   }
 }
 
-let pageNode = null
-let routerMounted = false
-function mountRouter(newPageNode) {
-  pageNode = newPageNode
-  if (routerMounted) {
-    return
+class Router {
+  constructor() {
+    this.pageNode = null
+    this.initialized = false
   }
 
-  routerMounted = true
+  mountPage(pageNode) {
+    this.pageNode = pageNode
 
-  if (document.attachEvent) {
-    document.attachEvent('onclick', onClickEvent)
-  } else {
-    document.addEventListener('click', onClickEvent, false)
-  }
-
-  window.history.replaceState(null, pageModuleName(window.location.href), window.location.href)
-  window.onpopstate = function onpopstate() {
-    loadPage(pageModuleName(window.location.href), window.location.href, true)
-  }
-}
-
-function onClickEvent(event) {
-  const target = event.target
-
-  if (target.tagName === 'A') {
-    const href = target.href
-
-    if (href.includes(window.location.origin)) {
-      event.preventDefault()
-
-      loadPage(pageModuleName(href), href)
+    if (!this.initialized) {
+      this.initialize()
+      this.initialized = true
     }
   }
-}
 
-function loadPage(pageModule, url, skipPushState) {
-  require([pageModule], (Component) => {
-    renderPage(Component, pageModule, url, skipPushState)
-  }, () => {
-    require(['pages/404'], (ErrorPage) => {
-      renderPage(ErrorPage, pageModule, url, skipPushState)
+  initialize() {
+    if (!this.isBrowserCompatibleWithRouter()) {
+      return
+    }
+
+    const { history, location } = window
+    const { href } = location
+
+    if (document.attachEvent) {
+      document.attachEvent('onclick', this.onClickEvent.bind(this))
+    } else {
+      document.addEventListener('click', this.onClickEvent.bind(this), false)
+    }
+
+    history.replaceState(null, this.pageModuleName(href), href)
+
+    const self = this
+    window.onpopstate = function onpopstate() {
+      self.loadPage(self.pageModuleName(href), href, true)
+    }
+  }
+
+  onClickEvent(event) {
+    const { target } = event
+
+    if (target.tagName === 'A') {
+      const { href } = target
+
+      if (href.includes(window.location.origin)) {
+        event.preventDefault()
+
+        this.loadPage(this.pageModuleName(href), href)
+      }
+    }
+  }
+
+  loadPage(pageModule, url, skipPushState) {
+    const self = this
+    require([pageModule], (Component) => {
+      self.renderPage(Component, pageModule, url, skipPushState)
+    }, () => {
+      require(['pages/404'], (ErrorPage) => {
+        self.renderPage(ErrorPage, pageModule, url, skipPushState)
+      })
     })
-  })
-}
+  }
 
-function renderPage(Component, pageModule, url, skipPushState) {
-  const pageElement = pageNode.base
-  const root = pageElement.parentNode
-  preact.render(preact.h(Component.default), root, pageElement)
+  renderPage(Component, pageModule, url, skipPushState) {
+    const { pageNode } = this
+    const { base: pageElement } = pageNode
+    const root = pageElement.parentNode
+    preact.render(preact.h(Component.default), root, pageElement)
 
-  if (!skipPushState) {
-    window.history.pushState(null, pageModule, url)
+    if (!skipPushState) {
+      window.history.pushState(null, pageModule, url)
+    }
+  }
+
+  pageModuleName(href) {
+    const moduleName = 'pages/' + href.replace(window.location.origin, '')
+      .replace(/^\//, '')
+      .replace(/\/$/, '')
+
+    return moduleName === 'pages/' ? 'pages/index' : moduleName
+  }
+
+  isBrowserCompatibleWithRouter() {
+    return window.location &&
+        window.history &&
+        typeof window.onpopstate !== 'undefined'
   }
 }
 
-function pageModuleName(href) {
-  const moduleName = 'pages/' + href.replace(window.location.origin, '')
-    .replace(/^\//, '')
-    .replace(/\/$/, '')
-
-  return moduleName === 'pages/' ? 'pages/index' : moduleName
-}
+router = new Router()
